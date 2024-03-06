25ccda570af59ef7a30d03470e949d9bac09494b
iamrohitg <rohitghosh76@gmail.com>
2024-03-07 00:55:18 +0530

    Services Update

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,48 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Breakfast',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
+      new Recipe(
+        'Delicious Pizza',
+        'A classic pizza with cheese and tomato sauce.',
+        'https://upload.wikimedia.org/wikipedia/commons/f/f4/Pizza.png',
+        [
+          new Ingredient('Dough', 1),
+          new Ingredient('Cheese', 1),
+          new Ingredient('Tomato Sauce', 1),
+          new Ingredient('Toppings', 10)
+        ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
