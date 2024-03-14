af05d3480ecfc0dbe85eeff6bc0a7588713e09f4
iamrohitg <rohitghosh76@gmail.com>
2024-03-14 17:40:55 +0530

    Angular Version 17 Update

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,19 +4,31 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { RecipeModule } from './recipes/recipe.module';
 
-
 const aapRoutes: Routes = [
   {
     path: '',
     redirectTo: '/recipes',
     pathMatch: 'full',
   },
-  { path: 'recipes', loadChildren: () => import('./recipes/recipe.module').then(m => m.RecipeModule)},
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipe.module').then((m) => m.RecipeModule),
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>
+      import('./shopping-list/shopping-list.module').then(
+        (m) => m.ShoppingListModule
+      ),
+  },
   { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(aapRoutes, { preloadingStrategy: PreloadAllModules })],
+  imports: [
+    RouterModule.forRoot(aapRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
